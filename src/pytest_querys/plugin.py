5bd8ac2989b1670f42b5a8f814aa0fb55bd844d9
biglanmao <biglanmao@outlook.com>
2025-11-30 21:48:38 +0800

    1、创建pytest插件，并在entry point注册

--- src/pytest_querys/plugin.py
+++ src/pytest_querys/plugin.py
@@ -1,2 +1,18 @@
+from pytest_querys.fixtures import (
+    services_inventory,
+    service_mangers,
+    service_querys,
+    service_sessions,
+)
+
+
 def pytest_addoption(parser):
-    parser.addini("service_inventory", help="Configure the service inventory path")
+    """Add options to control pytest-querys."""
+    group = parser.getgroup("pytest-querys")
+    group.addoption(
+        "--service-inventory",
+        action="store",
+        dest="service_inventory",
+        default="",
+        help="Configure the service inventory path",
+    )
